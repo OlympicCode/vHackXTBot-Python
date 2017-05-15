771a869ae861c8b74ce64c386900bf3822bda0ba
wookienz <y2r42ehg>
2017-05-15 19:15:25 +0800

    remove ut = Utils from each function def and place in class variable.

--- update.py
+++ update.py
@@ -3,29 +3,28 @@ from utils import Utils
 
 
 class Update:
+
+    ut= Utils()
+
     def getTasks(self):
-        ut = Utils()
-        temp = ut.requestString("user::::pass::::uhash",
+        temp =self. ut.requestString("user::::pass::::uhash",
                                 self.api.getUsername() + "::::" + self.api.getPassword() + "::::" + "userHash_not_needed",
                                 "vh_tasks.php")
         return temp
 
     def SpywareInfo(self):
-        ut = Utils()
-        arr = ut.requestArray("user::::pass::::uhash:::::",
+        arr = self.ut.requestArray("user::::pass::::uhash:::::",
                               self.api.getUsername() + "::::" + self.api.getPassword() + "::::" + "UserHash_not_needed" + ":::::",
                               "vh_spywareInfo.php")
         return arr
 
     def removeSpyware(self):
-        ut = Utils()
-        arr = ut.requestArray("user::::pass::::uhash:::::",
+        arr = self.ut.requestArray("user::::pass::::uhash:::::",
                               self.api.getUsername() + "::::" + self.api.getPassword() + "::::" + "UserHash_not_needed" + ":::::",
                               "vh_removeSpyware.php")
         return arr
 
     def getTaskAmount(self):
-        ut = Utils()
         temp = self.getTasks()
         return len(temp.split("taskid")) - 1
 
@@ -38,8 +37,7 @@ class Update:
         return n
 
     def startTask(self, type):
-        ut = Utils()
-        temp = ut.requestString("user::::pass::::uhash::::utype",
+        temp =self. ut.requestString("user::::pass::::uhash::::utype",
                                 self.api.getUsername() + "::::" + self.api.getPassword() + "::::" + "userHash_not_needed" + "::::" + type,
                                 "vh_addUpdate.php")
         if "result" in temp:
@@ -47,22 +45,19 @@ class Update:
         return "2"
 
     def botnetInfo(self):
-        ut = Utils()
-        temp = ut.requestString("user::::pass::::uhash",
+        temp =self. ut.requestString("user::::pass::::uhash",
                                 self.api.getUsername() + "::::" + self.api.getPassword() + "::::" + "userHash_not_needed",
                                 "vh_botnetInfo.php")
         return temp
 
     def upgradeBotnet(self, ID):
-        ut = Utils()
-        temp = ut.requestString("user::::pass::::uhash::::bID",
+        temp =self. ut.requestString("user::::pass::::uhash::::bID",
                                 self.api.getUsername() + "::::" + self.api.getPassword() + "::::" + "userHash_not_needed" + "::::" + ID,
                                 "vh_upgradeBotnet.php")
         return temp
 
     def finishTask(self, taskID):
-        ut = Utils()
-        temp = ut.requestString("user::::pass::::uhash::::taskid",
+        temp =self. ut.requestString("user::::pass::::uhash::::taskid",
                                 self.api.getUsername() + "::::" + self.api.getPassword() + "::::" + "userHash_not_needed" + "::::" + taskID,
                                 "vh_finishTask.php")
         if "4" in temp:
@@ -71,8 +66,7 @@ class Update:
             return False
 
     def finishAll(self):
-        ut = Utils()
-        temp = ut.requestString("user::::pass::::uhash",
+        temp =self. ut.requestString("user::::pass::::uhash",
                                 self.api.getUsername() + "::::" + self.api.getPassword() + "::::" + "userHash_not_needed",
                                 "vh_finishAll.php")
         if "0" in temp:
@@ -81,8 +75,7 @@ class Update:
             return False
 
     def useBooster(self):
-        ut = Utils()
-        temp = ut.requestString("user::::pass::::uhash::::boost",
+        temp =self. ut.requestString("user::::pass::::uhash::::boost",
                                 self.api.getUsername() + "::::" + self.api.getPassword() + "::::" + "userHash_not_needed" + "::::" + "1",
                                 "vh_tasks.php")
         return temp
