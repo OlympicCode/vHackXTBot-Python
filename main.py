f328628aa25ed699e79130faf1c74bb186d37621
vBlackOut <vladimir.souchet@cuby-hebergs.com>
2017-07-12 04:32:20 +0200

    add line "your are running numbertasks/totaltasks"

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ class run:
                         self.updatecount += 1
                         time.sleep(self.wait_load)
                         print "require " + str(moneyforupdate) + "$ for update " + self.updates[self.updatecount] + " your money " + str(mymoney) + "$"
-                        totaltask = int(self.running_all+self.updatecount)
+                        totaltask = int(self.running_all)+int(self.updatecount)
                         if totaltask == self.get_max_update:
                             stat = "0"
                     else:
@@ -69,7 +69,7 @@ class run:
                             #u.useBooster()
                             time.sleep(self.wait_load)
                             self.updatecount += 1
-                            totaltask = int(self.running_all+self.updatecount)
+                            totaltask = int(self.running_all)+int(self.updatecount)
                             if totaltask == self.get_max_update:
                                 stat = "0"
                                 while self.updatecount > 0:
