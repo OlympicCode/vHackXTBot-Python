9316a7c14539445238f07d5219f9ab40ca06af79
vBlackOut <vladimir.souchet@cuby-hebergs.com>
2017-09-01 07:08:52 +0200

    Optimised default bot

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ password = "password"
 # Enter Max Antivir to attack in normal mode
 maxanti_normal = 3000
 
-# Active or not the protection for cluster
+# Active or not the protection for cluster ( don't attack if you are ddos your cluster)
 active_cluster_protection = True
 
 # Use booster
@@ -27,12 +27,14 @@ attacks_tournament = 2
 # define the initial mode
 mode = "Secure"
 
+# update botnet auto
 BotNet_update = False
 
+# check list of more player in tournament and ddos the cluster.
 ddos_cluster = False
 
 # change auto mode Potator for tournament
-tournament_potator = True
+tournament_potator = false
 
 AttackTournamentDB = False
 Max_point_tournament = 1300
@@ -40,12 +42,16 @@ Max_point_tournament = 1300
 updates = ["ipsp", "adw", "fw", "scan", "sdk", "av"]
 # updates = ["fw"]
 # updates = ["ipsp",  "sdk"]
+
 # Do you want to attack during tournament [True, False]
 joinTournament = False
+
 # Time to wait between each cycle in seconds
 wait = round(uniform(0, 1), 2)
 wait_load = round(uniform(1, 3), 2)
 updatecount = 0
 attackneeded = False
 database = 'database.db'
+
+# check attack anonymous
 anon = True
